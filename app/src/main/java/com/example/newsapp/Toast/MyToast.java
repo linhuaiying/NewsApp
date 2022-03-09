4aeeea63cae98a1b32059627fbd6de0b2441aa30
linhuaiying <1274875417@qq.com>
2022-03-09 22:12:21 +0800

    避免异常

--- app/src/main/java/com/example/newsapp/Toast/MyToast.java
+++ app/src/main/java/com/example/newsapp/Toast/MyToast.java
@@ -6,7 +6,7 @@ import com.example.newsapp.Application.MyApplication;
 
 public class MyToast {
     public static void toast(String s) {
-        Toast toast = Toast.makeText(MyApplication.getContext(), null, Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(MyApplication.getContext(), "", Toast.LENGTH_SHORT);
         toast.setText(s);
         toast.show();
     }
