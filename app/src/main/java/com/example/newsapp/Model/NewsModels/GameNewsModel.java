4d99f5d91700c07a9eb249296af2d926e41364f9
linhuaiying <1274875417@qq.com>
2022-03-09 21:32:49 +0800

    fix

--- app/src/main/java/com/example/newsapp/Model/NewsModels/GameNewsModel.java
+++ app/src/main/java/com/example/newsapp/Model/NewsModels/GameNewsModel.java
@@ -3,6 +3,6 @@ package com.example.newsapp.Model.NewsModels;
 public class GameNewsModel extends NewsModel {
     @Override
     public void loadNewsData(NewsModel.OnLoadListener onLoadListener) throws InterruptedException {
-       onLoadListener.onComplete(getData("youxi"));
+        onLoadListener.onComplete(getData("youxi"));
     }
 }
