af6e10128322919545309fe30ff640ba1dcd032f
linhuaiying <1274875417@qq.com>
2022-03-12 18:40:04 +0800

    我的界面UI

--- app/src/main/java/com/example/newsapp/View/NewsView/MyFragment.java
+++ app/src/main/java/com/example/newsapp/View/NewsView/MyFragment.java
@@ -1,5 +1,6 @@
 package com.example.newsapp.View.NewsView;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,16 +8,18 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import com.example.newsapp.R;
 
 public class MyFragment extends Fragment {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = LayoutInflater.from(container.getContext()).inflate(R.layout.concern_news_layout, container, false);
+        View view = LayoutInflater.from(container.getContext()).inflate(R.layout.my_layout, container, false);
         return view;
     }
 }
