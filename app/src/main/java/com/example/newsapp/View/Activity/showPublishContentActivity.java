98987cda7cc55dc705f96901cbd0165c8be08e2a
linhuaiying <1274875417@qq.com>
2022-03-20 18:59:01 +0800

    设置新闻展示页面的UI

--- app/src/main/java/com/example/newsapp/View/Activity/showPublishContentActivity.java
+++ app/src/main/java/com/example/newsapp/View/Activity/showPublishContentActivity.java
@@ -1,33 +1,44 @@
 package com.example.newsapp.View.Activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.example.newsapp.R;
+import com.example.newsapp.bean.UsersNewsbean.UsersNews;
 
 public class showPublishContentActivity extends AppCompatActivity {
 
     WebView webView;
+    TextView title;
     String data;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_publish_content);
+        getWindow().setStatusBarColor(Color.argb(0, 211,211,211));
         webView = findViewById(R.id.newscontent);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
+        title = findViewById(R.id.title);
         data = getIntent().getStringExtra("data");
+        title.setText(getIntent().getStringExtra("title"));
         webView.loadDataWithBaseURL(null, data, "text/html" , "utf-8", null);
     }
 
-    public static void actionStart(Context context, String data) {
+    public static void actionStart(Context context, UsersNews usersNews) {
         Intent intent = new Intent(context, showPublishContentActivity.class);
-        intent.putExtra("data", data);
+        intent.putExtra("data", usersNews.getNewsContent());
+        intent.putExtra("title", usersNews.getTitle());
         context.startActivity(intent);
     }
 }
--- app/src/main/java/com/example/newsapp/adapter/UsersNewsAdapter.java
+++ app/src/main/java/com/example/newsapp/adapter/UsersNewsAdapter.java
@@ -8,15 +8,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.newsapp.Application.MyApplication;
-import com.example.newsapp.LocalUtils.SaveAccount;
 import com.example.newsapp.R;
 import com.example.newsapp.bean.UsersNewsbean.UsersNews;
 import com.example.newsapp.View.Activity.showPublishContentActivity;
 
 import java.util.List;
 
-public class UsersNewsAdapter extends RecyclerView.Adapter<UsersNewsAdapter.ConcernNewsViewHolder> {
+public class UsersNewsAdapter extends RecyclerView.Adapter<UsersNewsAdapter.UsersNewsViewHolder> {
     private List<UsersNews> usersNewsList;
 
     public UsersNewsAdapter(List<UsersNews> usersNewsList) {
@@ -24,22 +22,21 @@ public class UsersNewsAdapter extends RecyclerView.Adapter<UsersNewsAdapter.Conc
     }
     @NonNull
     @Override
-    public ConcernNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public UsersNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_news_item_layout, parent, false);
-        ConcernNewsViewHolder concernNewsViewHolder = new ConcernNewsViewHolder(view);
-        concernNewsViewHolder.concernNewsView.setOnClickListener(new View.OnClickListener() {
+        UsersNewsViewHolder usersNewsViewHolder = new UsersNewsViewHolder(view);
+        usersNewsViewHolder.concernNewsView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int position = concernNewsViewHolder.getAdapterPosition();
-                String content = usersNewsList.get(position).getNewsContent();
-                showPublishContentActivity.actionStart(parent.getContext(), content);
+                int position = usersNewsViewHolder.getAdapterPosition();
+                showPublishContentActivity.actionStart(parent.getContext(), usersNewsList.get(position));
             }
         });
-        return  concernNewsViewHolder;
+        return usersNewsViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ConcernNewsViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull UsersNewsViewHolder holder, int position) {
         UsersNews usersNews = usersNewsList.get(position);
         holder.title.setText(usersNews.getTitle());
         holder.time.setText(usersNews.getDate());
@@ -56,14 +53,14 @@ public class UsersNewsAdapter extends RecyclerView.Adapter<UsersNewsAdapter.Conc
         return usersNewsList.size();
     }
 
-    static class ConcernNewsViewHolder extends RecyclerView.ViewHolder{
+    static class UsersNewsViewHolder extends RecyclerView.ViewHolder{
 
         View concernNewsView;
         TextView title;
         TextView time;
         TextView nickName;
         View divide;
-        public ConcernNewsViewHolder(@NonNull View itemView) {
+        public UsersNewsViewHolder(@NonNull View itemView) {
             super(itemView);
             concernNewsView = itemView;
             title = itemView.findViewById(R.id.title);
